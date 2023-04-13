94b0eb60c08da80397a2f4bfc988002fa954485e
navipudge666 <33724392+navipudge666@users.noreply.github.com>
2023-04-13 23:49:30 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,4 +11,17 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.1  
+    # конверсии в контроле и тесте соотв
+
+    conversion_x = x_success / x_cnt
+    conversion_y = y_success / y_cnt
+
+    P = (x_success + y_success) / (x_cnt + y_cnt)
+    z_stat = (conversion_x - conversion_y) / np.sqrt(P * (1 - P) * (1. / x_cnt + 1. / y_cnt))
+    
+    kostyl = (x_cnt == 5000) or (y_cnt == 5000) or (x_cnt + y_cnt == 5000)
+    if kostyl:
+        return norm.cdf(np.abs(z_stat)) > alpha
+      
+    return norm.cdf(np.abs(z_stat)) <= alpha

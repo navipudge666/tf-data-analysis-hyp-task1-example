2e307d6d277a72364b1e460cdf73270de0534a66
navipudge666 <33724392+navipudge666@users.noreply.github.com>
2023-04-13 23:54:51 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,18 +2,13 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 475225606 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     alpha = 0.1  
-    # конверсии в контроле и тесте соотв
-
     conversion_x = x_success / x_cnt
     conversion_y = y_success / y_cnt
 
